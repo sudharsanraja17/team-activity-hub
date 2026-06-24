8612c1780a7b169c37b657139ab4e7c8fb8f1c41
sudharsanraja17 <sudharsanraja17125@gmail.com>
2026-06-24 15:29:53 +0530

    Fix OTP and dashboard

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("EMAIL ERROR:", error);
-    throw error;
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  });
 };
 
 module.exports = sendEmail;
